1eabe7ccacf782c74fe8c4ebeea422185fe90dc7
throw-Exceptions <ehdtls901@gmail.com>
2023-06-07 20:37:09 +0900

    feat: slide in out on press icon

--- src/chromeServices/content.ts
+++ src/chromeServices/content.ts
@@ -1,19 +1,28 @@
 import { createElement } from 'react';
 import ReactDOM from 'react-dom';
-import SlideBar from '../components/SlideBar';
-import { BAR_WIDTH } from '../constants/slidebar.constants';
+import SlideBar, { toggle } from '../components/SlideBar';
 import { ToggleMessage } from '../types';
+
+const ID = 'slide_bar_root';
 
 chrome.runtime.onMessage.addListener(
   (msg: ToggleMessage, sender: chrome.runtime.MessageSender) => {
     if (msg.type !== 'TOGGLE') return;
-    const div = document.createElement('div');
-    div.style.position = 'fixed';
-    div.style.top = '0px';
-    div.style.right = `0px`;
-    div.style.width = '0px';
-    div.style.height = '0px';
-    ReactDOM.render(createElement(SlideBar, null), div);
-    document.body.appendChild(div);
+    let div = document.getElementById(ID);
+    if (!div) {
+      div = document.createElement('div');
+      div.id = ID;
+      div.style.position = 'fixed';
+      div.style.top = '0px';
+      div.style.right = `0px`;
+      div.style.width = '0px';
+      div.style.height = '0px';
+      div.style.zIndex = '999';
+      ReactDOM.render(createElement(SlideBar, null), div);
+      document.body.appendChild(div);
+      setTimeout(toggle, 100);
+    } else {
+      toggle();
+    }
   },
 );
--- src/components/SlideBar.tsx
+++ src/components/SlideBar.tsx
@@ -1,29 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { BehaviorSubject } from 'rxjs';
 import { BAR_WIDTH } from '../constants/slidebar.constants';
 
 const SlideBar = () => {
   const [show, setShow] = useState(false);
+  const [height, setHeiht] = useState(window.innerHeight);
 
   useEffect(() => {
-    setTimeout(() => setShow(true), 100);
+    window.addEventListener('resize', () => {
+      setHeiht(window.innerHeight);
+    });
+    const s = toggleObservable.subscribe(setShow);
+    return () => s.unsubscribe();
   }, []);
 
   return (
     <div
       style={{
-        transition: '0.3s ease-out',
+        transition: 'transform 0.3s ease-out',
         transform: show ? `translateX(-${BAR_WIDTH}px)` : `translateX(0px)`,
         width: BAR_WIDTH,
-        height: window.innerHeight,
+        height,
         background: 'red',
         position: 'absolute',
         right: -BAR_WIDTH,
         top: 0,
       }}
     >
+      <button onClick={toggle}>Close</button>
       SlideBar
     </div>
   );
 };
 
 export default SlideBar;
+
+export const toggleObservable = new BehaviorSubject<boolean>(false);
+
+export const toggle = () => toggleObservable.next(!toggleObservable.getValue());
